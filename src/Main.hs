40e449da27617293c06759dde428cda8a3c7443e
Griffin Smith <wildgriffin45@gmail.com>
2015-07-16 13:48:22 -0400

    Render a guy!

--- src/Main.hs
+++ src/Main.hs
@@ -1,15 +1,32 @@
 module Main where
 
 import FRP.Helm
+import FRP.Helm.Signal (constant)
+import FRP.Helm.Keyboard
+import FRP.Helm.Color
 import qualified FRP.Helm.Window as Window
+import FRP.Elerea.Simple hiding (Signal)
 
-newtype Point = (Int, Int)
-                deriving (Eq, Ord, Read, Show)
+data GuyState = GuyState {gx :: Int, gy :: Int}
 
-render :: Point -> Element
-render (w, h) = collage w h [move (100, 100) $ filled red $ square 64]
+guyPos :: GuyState -> (Double, Double)
+guyPos guy =
+      (realToFrac . gx $ guy, realToFrac . gy $ guy)
+
+guySignal :: Signal GuyState
+guySignal = foldp newState initialState wasd
+   where initialState = GuyState {gx = 0, gy = 0}
+         newState :: (Int, Int) -> GuyState -> GuyState
+         newState (x, y) state = state {gx = gx', gy = gy'}
+            where gx' = gx state + x
+                  gy' = gy state + y
+
+render :: GuyState -> (Int, Int) -> Element
+render guy (w, h) =
+      collage w h [move (guyPos guy) $ filled red $ square 64]
 
 main :: IO ()
-main = do
-        engine <- startup defaultConfig
-        run engine $ render <~ Window.dimensions engine
+main =
+      let config = defaultConfig {windowTitle = "Bewp"}
+          dimensions = windowDimensions config in do
+             run config $ render <~ guySignal ~~ constant dimensions
